e1d9493bf150bab3f9a71d9bb37c18077fe1a540
Hement Sharma <hement.ece@gmail.com>
2016-12-11 13:01:22 +0100

    minor updates
    minor updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
  * Created by hementsharma on 09/12/16.
  */
 window.onload = function () {
-    console.log("test");
     document.querySelector(".menu").addEventListener("click", function(){
         this.classList.toggle( "active" );
         document.querySelector(".header-contact ul").classList.toggle( "list-active" );
